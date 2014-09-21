6a2b82a2fcbb0bd812ddb635600954cea61dac18
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-09-22 08:08:10 +0900

    change to use ftruncate(2) to truncate a file

--- fd_utils.c
+++ fd_utils.c
@@ -1,19 +1,12 @@
+#define _BSD_SOURCE
 #include "def.h"
 #include <unistd.h>
+#include <sys/types.h>
 #include <errno.h>
 #include <string.h>
 
 void fill_fd_with_zero(int fd, uint64_t size)
 {
-	uint64_t i;
-	uint8_t zero=0;
-	char errmsg[0xffff];
-
-	for(i=0; i<size; i++)
-		if(write(fd, &zero, 1)==-1){
-			sprintf(errmsg, "fill_fd_zero: write: %s", strerror(errno));
-			will_and_die(errmsg, 1);
-		}
-
+	ftruncate(fd, size);
 	return;
 }
