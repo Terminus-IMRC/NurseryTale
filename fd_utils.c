82c7e663a2b293f77b5e4f97949f71f4a2908619
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-10-01 08:36:26 +0900

    change to handle the return value of ftruncate

--- fd_utils.c
+++ fd_utils.c
@@ -7,6 +7,12 @@
 
 void fill_fd_with_zero(int fd, uint64_t size)
 {
-	ftruncate(fd, size);
+	char errmsg[0xffff];
+
+	if(ftruncate(fd, size)==-1){
+		sprintf(errmsg, "fill_fd_with_zero: ftruncate: %s\n", strerror(errno));
+		will_and_die(errmsg, 1);
+	}
+
 	return;
 }
